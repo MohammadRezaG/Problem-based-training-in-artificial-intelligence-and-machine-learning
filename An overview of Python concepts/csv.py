e273536c48671f5fd736cf70d72096a4375c956d
Mohammad reza golsorkhi <mgol2077@outlook.com>
2021-02-01 15:12:49 +0330

    csv

--- An overview of Python concepts/csv.py
+++ An overview of Python concepts/csv.py
@@ -1,62 +1,68 @@
 class Table:
     labels=[]
-    grows=[]
+    rows=[]
     def __init__(self, path=None):
-        global grows,labels
+        global rows,labels
+        if path == None:
+            labels=[]
+            rows=[]
+            return
         with open(path,'r') as file:
             file=file.read()
-            file=file.replace('\n',' ').split()
-            labels=file[0].split(',')
+            file=file.split('\n')
+            self.labels=file[0].split(',')
             for x in file[1:]:
-                grows.append(x.split(','))
+                self.rows.append(x.split(','))
     def save(self, path):
-        global grows,labels
+        global rows,labels
         t=[]
-        t.append(','.join(labels))
-        for x in grows:
+        t.append(','.join(self.labels))
+        for x in self.rows:
             t.append(','.join(x))
         t=('\n'.join(t))
         with open(path,'w') as file:
             file.write(t)
     def get_row_dict(self, row_index):
-        global grows,labels
+        global rows,labels
         c=0
         d={}
-        for x in labels:
-            d[x]=grows[row_index][c]
+        for x in self.labels:
+            d[x]=self.rows[row_index][c]
             c+=1
         return d
     def get_cell(self, row_index, label):
-        global grows,labels
-        return grows [row_index][labels.find(label)]
+        global rows,labels
+        return self.rows [row_index][self.labels.index(label)]
     def get_column(self, label):
-        global grows,labels
-        label=labels.find(label)
+        global rows,labels
+        label=self.labels.index(label)
         row=[]
-        for x in grows:
+        for x in self.rows:
             row.append(x[label])
+        return row
     def get_label_index(self, label):
-        global grows,labels
-        if label in labels:
-            return labels.find(label)
+        global rows,labels
+        if label in self.labels:
+            return self.labels.index(label)
         else : return 'KeyError'
 
     def to_dict(self):
-        global grows,labels
+        global rows,labels
         """
                 format: {
                             "labels":[],
-                            "grows":[[...],
+                            "rows":[[...],
                                     [...],
                                     .....
                                     .....
                                     [...]]
                         }
         """
-        dic={"labels":labels,"grows":grows}
+        dic={"labels":self.labels,"rows":self.rows}
         return dic
 
 t = Table("mytest.csv")
+t.save('mytest1.csv')
 print(t.to_dict()               )
 print(t.get_row_dict(0)         )
 print(t.get_column('number')    )
